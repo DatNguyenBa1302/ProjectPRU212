590cad185e29d1eb108dfa48e1632ad098573d98
NguyenBaDat <bmbadat2234@gmail.com>
2025-01-21 13:17:01 +0700

    Update map

---
+++ Assets/Script/AnimationSpriteRender.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class AnimationSpritesRender : MonoBehaviour
+{
+    private SpriteRenderer spriteRender;
+
+    public Sprite idleSprite;
+    public Sprite[] animationSprites;
+    public float animationTime = 0.25f;
+    private int animationFrame;
+
+    public bool loop = true;
+    public bool idle = true;
+    public void Awake()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+        Debug.Log(">>>> spriteRender " + spriteRender);
+
+    }
+
+    private void OnEnable()
+    {
+        spriteRender.enabled = true;
+    }
+    private void OnDisable()
+    {
+        spriteRender.enabled = false;
+    }
+    private void Start()
+    {
+        InvokeRepeating(nameof(NextFrame), animationTime, animationTime);
+    }
+
+    private void NextFrame()
+    {
+        animationFrame++;
+        if(loop && animationFrame >= animationSprites.Length) 
+        {
+            animationFrame = 0;
+        }
+        if (idle)
+        {
+            spriteRender.sprite = idleSprite;
+        }else if(animationFrame >= 0 && animationFrame < animationSprites.Length) { }
+        {
+            spriteRender.sprite = animationSprites[animationFrame];
+        }
+    }
+}
---
+++ Assets/Script/BombController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    public KeyCode inputKey = KeyCode.Space;
+    public float bombFuseTime = 3f;
+    public int bombAmount = 1;
+
+    private int bombsRemaining = 0;
+
+    private void OnEnable()
+    {
+        bombsRemaining = bombAmount;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(inputKey) && bombsRemaining > 0) 
+        {
+            StartCoroutine(PlaceBomb());
+        }
+    }
+
+    private IEnumerator PlaceBomb()
+    {
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        GameObject bomb =  Instantiate(bombPrefab, position, Quaternion.identity);
+        bombsRemaining--;
+
+        yield return new WaitForSeconds(bombFuseTime);
+
+        Destroy(bomb );
+        bombsRemaining++;   
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bomb"))
+        {
+            other.isTrigger = false;
+        }
+    }
+     
+}
